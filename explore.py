c1b75a341f24504a23e66c502dda4b7b534339a4
Andrew Enfield <aenfield@gmail.com>
2025-05-18 15:15:09 -0700

    Added Python API demo of tool usage functionality.

--- explore.py
+++ explore.py
@@ -46,17 +46,38 @@ import llm
 
 # asyncio.run(main())
 
-# structured data extraction
-import json
-from pydantic import BaseModel
+# # structured data extraction
+# import json
+# from pydantic import BaseModel
 
-class Pelican(BaseModel):
-    name: str
-    age: int
-    short_bio: str
-    beak_capacity_ml: float
+# class Pelican(BaseModel):
+#     name: str
+#     age: int
+#     short_bio: str
+#     beak_capacity_ml: float
 
+# model = llm.get_model('gpt-4.1-mini')
+# response = model.prompt('Describe a spectacular pelican', schema=Pelican)
+# pelican = json.loads(response.text())
+# print(pelican)
+
+# calling tools
 model = llm.get_model('gpt-4.1-mini')
-response = model.prompt('Describe a spectacular pelican', schema=Pelican)
-pelican = json.loads(response.text())
-print(pelican)
+
+def lookup_population(country: str) -> int:
+    "Returns the current population of the specified fictional country"
+    if country == 'Crumpet':
+        return 123124
+    else:
+        return 42
+
+def can_have_dragons(population: int) -> bool:
+    "Returns True if the specified population can have dragons, False otherwise"
+    return population > 10000
+
+chain_response = model.chain(
+    "Can the country of Crumpet have dragons? What about the country of Foo? What about the USA? What about France? Answer with only YES or NO",
+    tools=[lookup_population, can_have_dragons],
+    stream=False
+)
+print(chain_response.text())
